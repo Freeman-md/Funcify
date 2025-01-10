31cd130b646948d3714d2b28044cfa2ef0981e56
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-01-10 14:00:34 +0000

    refactor: remove invalid input tests for CreateItem method in CosmosDBServiceTests

--- Funcify.Tests/Services/CosmosDBServiceTests.cs
+++ Funcify.Tests/Services/CosmosDBServiceTests.cs
@@ -198,29 +198,6 @@ public class CosmosDBServiceTests
         #endregion
     }
 
-    [Theory]
-    [InlineData([null, 90.9, 9])]
-    [InlineData(["", 90.9, 9])]
-    [InlineData(["Product Name", null, 9])]
-    [InlineData(["Product Name", 0, 9])]
-    [InlineData(["Product Name", -1, 9])]
-    [InlineData(["Product Name", 90.9, null])]
-    [InlineData(["Product Name", 90.9, -1])]
-    public async Task CreateItem_WhenInValidInputsAreProvided_ShouldThrowArgumentException(string name, decimal price, int quantity)
-    {
-        #region Arrange
-        Product product = new ProductBuilder()
-                                .WithName(name)
-                                .WithPrice(price)
-                                .WithQuantity(quantity)
-                                .Build();
-        #endregion
-
-        #region Act & Assert
-        await Assert.ThrowsAsync<ArgumentException>(async () => await _cosmosDBService.CreateItem<Product>("Database", "Container", product));
-        #endregion        
-    }
-
     [Fact]
     public async Task CreateItem_WhenItemCreationFails_ShouldThrowCosmosException()
     {
